25c3b0f6f0c4fe5d7b67a77d11c27a19878b4bc7
atefeh97hmt <hemmati.atefeh97@gmail.com>
2023-02-09 10:58:34 +0330

    delete some not used lines

--- MyExercises/Program.cs
+++ MyExercises/Program.cs
@@ -3,59 +3,34 @@ using System.Collections.Generic;
 using System.IO;
 
 
-class MyApp { 
-static void Main()
+class MyApp
 {
-    string YourName = "";
-    var filePath = @"E:\names.txt";
-    using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+    static void Main()
     {
-        using (StreamReader sr = new StreamReader(fs))
+        string YourName = "";
+        var filePath = @"E:\names.txt";
+        using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
         {
-            string content = sr.ReadToEnd();
-            string[] lines = content.Split(new string[] { Environment.NewLine },
-                StringSplitOptions.RemoveEmptyEntries);
-
-            int lineCount = 0;
-            List<Account> accounts = new List<Account>();
-            foreach (string line in lines)
+            using (StreamReader sr = new StreamReader(fs))
             {
-                string[] column = line.Split(',');
-                if (lineCount != 0)
-                {
-                    Account account = new Account();
-                    account.AddedName = column[0];
-                    accounts.Add(account);
-                }
-                lineCount++;
+                string content = sr.ReadToEnd();
+                string[] lines = content.Split(new string[] { Environment.NewLine },
+                    StringSplitOptions.RemoveEmptyEntries);
+                Console.WriteLine(content);
             }
-            Console.WriteLine(content);
         }
-    }
 
-    using (FileStream fs = new FileStream(filePath, FileMode.Append))
-    {
-        using (StreamWriter sw = new StreamWriter(fs))
+        using (FileStream fs = new FileStream(filePath, FileMode.Append))
         {
-            Account account = new Account();
-            account.AddedName = YourName;
-            Console.WriteLine("please enter your name to the list above:");
-            YourName = Console.ReadLine();
-            string fullText = (YourName);
-            sw.WriteLine(fullText);
-            Console.ReadLine();
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                Console.WriteLine("please enter your name to the list above:");
+                YourName = Console.ReadLine();
+                string fullText = (YourName);
+                sw.WriteLine(fullText);
+                Console.ReadLine();
+            }
         }
-    }
 
-}
-public class Account
-    {
-        public string AddedName { get; set; }
-
-        public static string GetAccountCSV(Account account)
-        {
-            string returnValue = account.AddedName;
-            return returnValue;
-        }
     }
 }
